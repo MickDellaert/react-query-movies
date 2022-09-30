0aa645eb716ded4414f474db6dba9ff132157d6f
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-09-30 19:45:44 +0200

    react-query testing

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,11 @@
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
 import axios from "axios";
 
 const BASE_URL = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
 });
+
+export const IMG_URL = "https://image.tmdb.org/t/p/w500/"
 
 export const getPopular = async () => {
   const response = await BASE_URL.get(
@@ -10,3 +13,23 @@ export const getPopular = async () => {
   );
   return response.data;
 };
+
+export const getTrending = async () => {
+  const response = await BASE_URL.get(
+    `trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
+  );
+  return response.data;
+};
+
+
+export const getMovies = async ({queryKey}) => {
+  console.log(queryKey)
+  // const [key, url] = queryKey;
+  const url = queryKey[1];
+
+  const response = await BASE_URL.get(
+    url
+  );
+  return response.data;
+};
+
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,17 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { getPopular } from "../api/api";
+import { getPopular, getTrending, getMovies, IMG_URL } from "../api/api";
 
 export const Movies = () => {
   // const queryClient = useQueryClient()
 
-  const { isLoading, data } = useQuery(["popular-movies"], getPopular);
+  const { isLoading, data } = useQuery(["trending-movies", `trending/all/week?api_key=d4208ada7ca0b82909b61d7d3afd76fa`], getMovies);
+  const { isLoading: popularLoading, data: popularData } = useQuery(
+    [
+      "popular-movies",
+      `movie/popular?api_key=d4208ada7ca0b82909b61d7d3afd76fa&language=en-US&page=1`,
+    ],
+    getMovies
+  );
 
   if (isLoading) {
     return <h2>Loading</h2>;
@@ -14,9 +21,20 @@ export const Movies = () => {
   return (
     <>
       <h2>Popular Movies</h2>
-      {data.results.map((popular) => {
-        return <h3 key={popular.id}>{popular.original_title}</h3>;
-      })}
+      {popularData?.results.map((popular) => (
+        <div key={popular.id}>
+          <h3>{popular.title}</h3>
+          <img src={`${IMG_URL}${popular.poster_path}`} />
+        </div>
+      ))}
+
+      <h2>Trending Movies</h2>
+      {data?.results.map((popular) => (
+        <div key={popular.id}>
+          <h3>{popular.title}</h3>
+          <img src={`${IMG_URL}${popular.poster_path}`} />
+        </div>
+      ))}
     </>
   );
 };
