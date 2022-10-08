089ad22a65d1b24a6f76c937f99f945727ee42e1
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-10-08 21:39:28 +0200

    working on featured slider

--- src/pages/home/TrendingItem.jsx
+++ src/pages/home/TrendingItem.jsx
@@ -2,61 +2,105 @@ import "./home.css";
 
 import * as api from "../../api/api";
 
-import { useEffect } from "react";
+import { useEffect, useState, useRef, useLayoutEffect } from "react";
 
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
 
 export const TrendingItem = ({ trendingData }) => {
+  const sizeRef = useRef();
+
+  const [clickedIndex, setClickedIndex] = useState();
+
   const [nextIndex, setNextIndex] = useState(0);
-  const [start, setStart] = useState(-220);
+  const [start, setStart] = useState(-200);
   const [style, setStyle] = useState({ top: `${start}px` });
+
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
 
   const trendingHeader = trendingData.results[nextIndex];
   const trendingNext = trendingData.results[nextIndex + 1];
 
-  const sliced = trendingData?.results.slice(0, 7);
-  const double = [...sliced, ...sliced];
+  const sliced = trendingData.results.slice(0, 10);
+  const double = [...sliced];
+
+  const doubleIndexed = double.map((item, key) => ({ key, ...item }));
+  console.log(doubleIndexed);
+
+  // sliced.push(sliced[0]);
+  // sliced.unshift(sliced[sliced.length - 2])
+  // const cloned = tempArr.unshift(sliced[sliced.length - 2])
+
+  // console.log(sliced[0]);
+  // console.log(sliced[8]);
+
+  const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  // const testSlice = test.slice(0,5)
+  // console.log(tempArr)
+  // console.log(sliced)
+
+  console.log("index"+ nextIndex);
+  // console.log(double.length);
 
   const id = trendingData.results[nextIndex].id;
   const type = trendingData.results[nextIndex].media_type;
-
 
   useEffect(() => {
     setStyle({ top: `${start}px` });
   }, [start]);
 
-  console.log(start);
-  console.log(style);
+  useLayoutEffect(() => {
+    if (sizeRef.current) {
+      setWidth(sizeRef.current.offsetWidth);
+      setHeight(sizeRef.current.offsetHeight);
+    }
+  });
+
+  // useEffect(() => {
+  //   console.log("effect" + height);
+  // }, [height]);
+
+  // console.log("height" + height);
+
+  // console.log(start);
+  // console.log(style);
 
   const nextFunction = () => {
-    if (nextIndex < 5) {
-      setNextIndex(nextIndex + 1);
-      setStart(start - 220);
-    }
+    setNextIndex(nextIndex + 1);
+    setStart(start - height);
 
-    if (nextIndex === 5) {
+    if (nextIndex === double.length / 2 - 1) {
       setNextIndex(0);
-      setStart(start + 1100);
+      setStart(-height);
     }
   };
+
+  const handleClick = (item) => {
+    // {setClickedIndex(event.target.id)}
+    setNextIndex(item);
+    setStart(-height * (item+1));
+    console.log("key" + item);
+  };
+
+  // useEffect(()=>{
+  //   console.log("clicked" + clickedIndex)
+
+  //   setClickedIndex(clickedIndex)
+  // })
 
   const previousFunction = () => {
-    if (nextIndex > 0) {
-      setNextIndex(nextIndex - 1);
-      setStart(start + 220);
-    }
+    setNextIndex(nextIndex - 1);
+    setStart(start + height);
     if (nextIndex === 0) {
-      setNextIndex(5);
-      setStart(start - 1100);
+      setNextIndex(double.length / 2 - 1);
+      setStart((-height * double.length) / 2);
     }
   };
-
 
   const { data: details, isLoading: detailsLoading } = useQuery(
     ["getDetails", id, type],
     () => api.getDetails(id, type)
-
   );
 
   if (detailsLoading) {
@@ -81,10 +125,18 @@ export const TrendingItem = ({ trendingData }) => {
 
       <div className="small-list-container">
         <div className="small-list-slider" style={style}>
-          {double.map((item) => (
+          {doubleIndexed.map((item) => (
             <>
-              <div className="small-list-item" key={item.id} >
-                <h3>{item.title}</h3>
+              <div
+                className="small-list-item"
+                key={item.id}
+                ref={sizeRef}
+                onClick={() => handleClick(item.key)}
+              >
+                {/* <p >{item.key} </p> */}
+                <h5>{item.title} </h5>
+                <h5>{item.original_name}</h5>
+
                 <img
                   className="small-list-image"
                   src={`${api.IMG_URL}${item.backdrop_path}`}
