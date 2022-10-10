92d4878b96e5355354318b0d8ceb808af3b0add7
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-10-10 15:57:37 +0200

    infinite sliding

--- src/pages/home/TrendingItem.jsx
+++ src/pages/home/TrendingItem.jsx
@@ -7,26 +7,22 @@ import { useEffect, useState } from "react";
 // import { useQuery, useQueries } from "@tanstack/react-query";
 
 export const TrendingItem = ({ trendingData }) => {
-  let divHeight = 1000;
-  let itemNumber = 6;
-  let containerPadding = 20;
+  let divHeight = 800;
+  let startItem = 3;
+  let itemNumber = 4;
+  let containerPadding = 0;
   let singlePadding = 5;
 
-  const [nextIndex, setNextIndex] = useState(0);
-
-  const [startOffset, setStartOffset] = useState(0);
-
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [singleHeight, setSingleHeight] = useState(
     (divHeight - containerPadding * 2) / itemNumber
   );
-
-  // const [transform, setTransform] = useState({ transform: `translateY(${startOffset}px)` });
-
+  const [startOffset, setStartOffset] = useState(0);
+  const [isForward, setisForward] = useState(false);
   const [clickedIndex, setClickedIndex] = useState();
+  const [transition, setTransition] = useState(true);
 
-  const [transition, setTransition] = useState();
-
-  const sliced = trendingData.results.slice(0, 10);
+  const sliced = trendingData.results.slice(0, 6);
   const double = [...sliced, ...sliced];
   const doubleIndexed = double.map((item, key) => ({ key, ...item }));
 
@@ -47,58 +43,68 @@ export const TrendingItem = ({ trendingData }) => {
   // console.log(tempArr)
   // console.log(sliced)
 
-  const id = trendingData.results[nextIndex].id;
-  const type = trendingData.results[nextIndex].media_type;
+  const id = trendingData.results[currentIndex].id;
+  const type = trendingData.results[currentIndex].media_type;
 
-  useEffect(() => {
-    setStartOffset(startOffset);
-  }, [startOffset]);
-
-  console.log("start" + startOffset);
+  // useEffect(() => {
+  //   setStartOffset(startOffset);
+  // }, [startOffset]);
 
   const nextFunction = () => {
-    if (nextIndex < doubleIndexed.length - 1) {
-      setNextIndex(nextIndex + 1);
+    setisForward(true);
+
+    if (currentIndex < doubleIndexed.length) {
+      setCurrentIndex((nextIndex) => nextIndex + 1);
       setStartOffset(startOffset - singleHeight);
     }
 
-    if (nextIndex === doubleIndexed.length / 2) {
-      setNextIndex(0);
+    if (currentIndex === doubleIndexed.length / 2) {
+      setCurrentIndex(0);
+      setTransition(false);
       setStartOffset(0);
     }
   };
 
   const previousFunction = () => {
-    if (nextIndex > 0) {
-      setNextIndex((nextIndex) => nextIndex - 1);
+    setisForward(false);
+
+    if (currentIndex > 0) {
+      setCurrentIndex((nextIndex) => nextIndex - 1);
       setStartOffset(startOffset + singleHeight);
     }
 
-    if (nextIndex === 0) {
-      setNextIndex(doubleIndexed.length / 2);
+    if (currentIndex === 0) {
+      setCurrentIndex(doubleIndexed.length / 2);
+      setTransition(false);
       setStartOffset((-singleHeight * doubleIndexed.length) / 2);
-      // setNextIndex(nextIndex => nextIndex - 1);
-      // setStartOffset(startOffset + singleHeight);
-      // setStartOffset(startOffset + singleHeight);
     }
   };
 
   useEffect(() => {
-    if (nextIndex === doubleIndexed.length / 2) {
+    if (!isForward && currentIndex === doubleIndexed.length / 2) {
+      setCurrentIndex((nextIndex) => nextIndex - 1);
+      setTransition(true);
       setStartOffset(startOffset + singleHeight);
     }
-  }, [nextIndex]);
+    if (isForward && currentIndex === 0) {
+      setCurrentIndex((nextIndex) => nextIndex + 1);
+      setTransition(true);
+      setStartOffset(-singleHeight);
+    }
+  }, [currentIndex]);
 
   const handleClick = (item) => {
-    setNextIndex(item);
+    setCurrentIndex(item);
     setStartOffset(-singleHeight * (item + 1));
     console.log("key" + item);
   };
 
-  console.log("index" + nextIndex);
+  console.log("index" + currentIndex);
   console.log("length" + doubleIndexed.length);
   console.log("start" + startOffset);
+  console.log("singleHeight" + singleHeight);
 
+  console.log(isForward);
 
   return (
     <>
@@ -123,7 +129,10 @@ export const TrendingItem = ({ trendingData }) => {
         <div className="vertical-slider-crop">
           <div
             className="vertical-slider-content"
-            style={{ transform: `translateY(${startOffset}px)` }}
+            style={{
+              transform: `translateY(${startOffset}px)`,
+              transition: transition ? `all 250ms linear` : "none",
+            }}
           >
             {doubleIndexed.map((item) => (
               <>
