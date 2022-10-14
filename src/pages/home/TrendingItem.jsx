7aa74d07a69bb30ff2710a22e1f1dbdbd961a634
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-10-14 10:16:17 +0200

    always scroll up when clicked

--- src/pages/home/TrendingItem.jsx
+++ src/pages/home/TrendingItem.jsx
@@ -9,7 +9,8 @@ import { useEffect, useState } from "react";
 export const TrendingItem = ({ trendingData }) => {
   let divHeight = 800;
   let startItem = 1;
-  let itemNumber = 5;
+  let itemNumber = 6;
+  let totalNumber = 12;
   let containerPadding = 10;
   let singlePadding = 5;
 
@@ -25,22 +26,24 @@ export const TrendingItem = ({ trendingData }) => {
   const [transition, setTransition] = useState(true);
   const [clicked, setIsClicked] = useState(false);
 
-  const sliced = trendingData.results.slice(0, 6);
-  const double = [...sliced, ...sliced];
-  const doubleIndexed = double.map((item, key) => ({ key, ...item }));
+  const sliced = trendingData.results.slice(0, totalNumber);
+  const double = [...sliced, ...sliced, ...sliced];
+
+  const slicedIndexed = sliced.map((item, key) => ({ key, ...item }));
+  const tripleIndexed = double.map((item, key) => ({ key, ...item }));
 
   // const trendingHeader = sliced[nextIndex];
   // const trendingNext = sliced[nextIndex + 1];
   // const doubleIndexed = double.map((item, key) => ({ key, ...item }));
 
-  console.log(doubleIndexed);
+  console.log(tripleIndexed);
 
-  useEffect(() => {
-    if (clicked) {
-      setTransition(true);
-      setCurrentIndex(clickedIndex - doubleIndexed.length / 2 );
-    }
-  }, [clicked]);
+  // useEffect(() => {
+  //   if (clicked) {
+  //     setTransition(true);
+  //     setCurrentIndex(clickedIndex - doubleIndexed.length / 2);
+  //   }
+  // }, [clicked]);
 
   const nextFunction = () => {
     setTransition(true);
@@ -55,26 +58,31 @@ export const TrendingItem = ({ trendingData }) => {
   const handleClick = (item) => {
     setTransition(true);
     setClickedIndex(item);
-    setCurrentIndex(item);
+    setCurrentIndex(item + 1);
     setIsClicked(false);
 
-    if (item > doubleIndexed.length / 2) {
-      setTransition(false);
-      setIsClicked(true);
-      setClickedIndex(item);
-      setCurrentIndex(0);
-    }
- };
+    // if (item > doubleIndexed.length) {
+    //   setTransition(false);
+    //   setIsClicked(true);
+    //   setClickedIndex(item);
+    //   setCurrentIndex(0);
+    // }
+  };
 
   const handleTransition = () => {
     if (!clicked) {
-      if (currentIndex === doubleIndexed.length / 2 + 1) {
-        setTransition(false);
-        setCurrentIndex(1);
-      }
+      // if (currentIndex === doubleIndexed.length + 1) {
+      //   setTransition(false);
+      //   setCurrentIndex(1);
+      // }
       if (currentIndex === 0) {
         setTransition(false);
-        setCurrentIndex(doubleIndexed.length / 2);
+        setCurrentIndex(tripleIndexed.length / 3);
+      }
+
+      if (currentIndex > tripleIndexed.length / 3) {
+        setTransition(false);
+        setCurrentIndex(currentIndex - tripleIndexed.length / 3);
       }
     }
 
@@ -115,11 +123,35 @@ export const TrendingItem = ({ trendingData }) => {
             className="vertical-slider-content"
             style={{
               transform: `translateY(${-currentIndex * singleHeight}px)`,
-              transition: transition ? `all 2500ms linear` : "none",
+              transition: transition ? `all 1000ms linear` : "none",
             }}
             onTransitionEnd={() => handleTransition()}
           >
-            {doubleIndexed.map((item) => (
+            {/* {slicedIndexed.map((item) => (
+              <>
+                <div
+                  className="vertical-slider-item sliced"
+                  key={item.id}
+                  onClick={() => handleClick(item.key)}
+                  style={{
+                    height: `calc((100%  / ${itemNumber})`,
+                    paddingBlock: `${singlePadding}px`,
+                  }}
+                >
+                  <p>{item.key} </p>
+                  <h5>{item.title} </h5>
+                  <h5>{item.original_name}</h5>
+
+                  <img
+                    className="vertical-slider-item-image"
+                    src={`${api.IMG_URL}${item.backdrop_path}`}
+                    alt=""
+                  />
+                </div>
+              </>
+            ))} */}
+
+            {tripleIndexed.map((item) => (
               <>
                 <div
                   className="vertical-slider-item"
